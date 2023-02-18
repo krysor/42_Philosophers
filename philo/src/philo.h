21551d32b93fc02b619fd2c970ef164c56cfed66
Krystian Kaczorowski <kkaczoro@mi-r8-p3.s19.be>
2023-02-18 17:57:25 +0100

    deleted executable

--- philo/src/philo.h
+++ philo/src/philo.h
@@ -6,27 +6,27 @@
 /*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 14:43:30 by kkaczoro          #+#    #+#             */
-/*   Updated: 2023/02/18 17:41:53 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2023/02/18 17:48:40 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# define NUMBER_OF_PHILOSOPHERS 1
-# define TIME_TO_DIE 2
-# define TIME_TO_EAT 3
-# define TIME_TO_SLEEP 4
-# define NUMBER_OF_TIMES_EACH_PHILOSOPHER_MUST_EAT 5
+# define NUMBER_OF_PHILOSOPHERS 					1
+# define TIME_TO_DIE 								2
+# define TIME_TO_EAT 								3
+# define TIME_TO_SLEEP 								4
+# define NUMBER_OF_TIMES_EACH_PHILOSOPHER_MUST_EAT 	5
 
-# define RED	"\x1B[31m"
-# define GREEN	"\x1B[32m"
-# define YELLOW	"\x1B[33m"
-# define BLUE	"\x1B[34m"
+# define RED		"\x1B[31m"
+# define GREEN		"\x1B[32m"
+# define YELLOW		"\x1B[33m"
+# define BLUE		"\x1B[34m"
 # define MAGENTA	"\x1B[35m"
-# define CYAN	"\x1B[36m"
-# define WHITE	"\x1B[37m"
-# define RESET	"\x1B[0m"
+# define CYAN		"\x1B[36m"
+# define WHITE		"\x1B[37m"
+# define RESET		"\x1B[0m"
 
 # include <stdio.h>
 

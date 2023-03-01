38f1c6962e3bfdeff1d6f84c530ff5ca9601f98d
Krystian Kaczorowski <kkaczoro@mi-r1-p3.s19.be>
2023-03-01 17:25:43 +0100

    switched back to array but this time heap allocated

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 12:06:13 by kkaczoro          #+#    #+#             */
-/*   Updated: 2023/02/28 17:05:12 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2023/03/01 17:19:10 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ void	*routine(void *pnt)
 	t_vars	*vars;
 
 	vars = (t_vars *)pnt;
-	printf("im a philo but idk which one\n");
+	printf("im a philo\n");
 	
 	while (!vars->dead)
 	{
 		vars->dead = 1;
 	}
+	printf("philo returning\n");
 	return (NULL);
 }
